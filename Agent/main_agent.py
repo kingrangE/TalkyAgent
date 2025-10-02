c941d3177abe45e6ad2bfc2db7ee27d29fd09dc4
kingrangE <yuvw123@gmail.com>
2025-10-02 19:26:06 +0900

    Modify : Modifying code related to LLM
    LLM 사용하는 부분을 LLM 객체를 이용하도록 변경

--- Agent/main_agent.py
+++ Agent/main_agent.py
@@ -14,12 +14,12 @@ Main Agent가 하는 일
     - 적절 시 응답 : (False, None)
     - 부적절 시 응답 : (True, 부적절한 이유)
 """
-
 import json
 from outlines import Generator
 import outlines
 import os
 import torch
+from llm import LLM
 from huggingface_hub import snapshot_download
 from langchain_community.llms import Outlines
 from pydantic import BaseModel, Field
@@ -72,121 +72,104 @@ class EvaluationResult(BaseModel):
 class MainAgent:
     def __init__(self):
         # exaone 없으면 download
-        if "exaone-4" not in os.listdir():
-            snapshot_download(repo_id="LGAI-EXAONE/EXAONE-4.0-1.2B",local_dir="exaone-4")
-        self.model = AutoModelForCausalLM.from_pretrained("exaone-4",dtype=torch.float16,device_map="auto",)
-        self.tokenizer = AutoTokenizer.from_pretrained("exaone-4")
+        self.llm = LLM()
         self.evaluate_model = outlines.from_transformers(
-            self.model,
-            self.tokenizer
+            self.llm.model,
+            self.llm.tokenizer
         )
-        self.model.eval()
         self.messages = [{"role":"system","content":TALK_PROMPT}]
 
-    def execute(self):
+    def execute(self,message):
         """전체 로직, 입력 받고 생각->행동 반복"""
         while True:
-            message = input("user :")
             if message == "quit" or message == "q":
                 exit(0)
-            # intention = self.think(message)
+            # intention = self.__think(message)
             intention = int(input())
-            self.act(intention,message)
+            self.__act(intention,message)
             
-    def think(self,message:str) -> int:
+    def __think(self,message:str) -> int:
         """의도 파악을 위한 생각 단계, 이후에 여기는 Adapter를 이용한 예측 또는 Model을 이용한 분류로 대체 예정"""
         intention = self.analysis_intention(message = message) # 의도 분석
         return intention
     
-    def act(self, intention: int, message: str) :
+    def __act(self, intention: int, message: str) :
         """의도에 따른 행동 분류"""
         if intention == 1 :
             # 현재 메시지를 전달하여 답변 얻기
-            self.talk(message=message)
+            self.__talk(message=message)
 
         elif intention == 2 :
             # 현재 보낸 메시지를 번역
-            print(self.translate(message=message))
+            print(self.__translate(message=message))
 
         elif intention == 3 :
             # 이전에 보낸 user의 답변 전송
-            print(self.improve_expression(message=self.messages[-2]) )
+            print(self.__improve_expression(message=self.messages[-2]) )
             """
             개선 : 
             talk에서 Improve_expression이 내장되면 좋을듯?
             """
         else :
             # 현재 메시지가 피드백이므로 현재 메시지를 전달
-            print(self.save_feedback(message=message))
+            print(self.__save_feedback(message=message))
 
-    def talk(self,message: str)-> None:
+    def __talk(self,message: str)-> None:
         """User와 대화를 진행하는 함수"""
         self.messages.append({"role":"user","content":message}) # 사용자 입력 메시지 추가
         # chat template 변환
-        input_ids = self.tokenizer.apply_chat_template(self.messages,add_generation_prompt=True, return_tensors="pt").to(self.model.device)
-        # 생성
-        with torch.no_grad():
-            output = self.model.generate(
-                input_ids,
-                max_new_tokens = len(input_ids)+512,
-                do_sample = False
-            )
-
-        # 결과 decoding & extract output
-        result = self.tokenizer.decode(output[0],skip_special_tokens=True)
-        result = result.split('</think>')[-1].strip()
-
+        result = self.llm.invoke(self.messages)
         # 대화 내역에 추가
         self.messages.append({"role":"assistant","content":result})
         print("AI :",result)
 
-    def translate(self,message: str) -> str :
+    def __translate(self,message: str) -> str :
         """한국어로 말한 문장을 영어로 번역해주는 단계"""
         TRANSLATION_EVALUATION_PROMPT="지금까지의 했던 대화를 토대로 보았을 때, 다음 문장이 자연스러운 답변인지 알려주세요."
         prompt = [{"role":"system","content":f"당신은 영어 통번역 전문가입니다. 다음 대사를 영어권 사람들이 일상적으로 구사하는 표현을 사용하여 영어로 번역해주세요. \n대사 : {message}"}]
-        result = self.invoke(prompt)
-        success,reason = self.evaluate(prompt=TRANSLATION_EVALUATION_PROMPT,sentence_to_evaluation=result)
-
-        if not success :
+        result = self.llm.invoke(prompt)
+        while True:
+            success,reason = self.__evaluate(prompt=TRANSLATION_EVALUATION_PROMPT,sentence_to_evaluation=result)
+            if success :
+                break
             prompt.append({"role":"assistant","content":result})
             prompt.append({"role":"user","content":f"당신의 번역은 다음의 이유로 부적절합니다. 이유를 고려하여 개선한 번역본을 전달해주세요.\n이유: {reason}"})
-            result = self.invoke(prompt,thinking=True)
-            success,reason = self.evaluate(prompt=TRANSLATION_EVALUATION_PROMPT,sentence_to_evaluation=result)
+            result = self.llm.invoke(prompt,thinking=True)
 
         return result
     
-    def improve_expression(self, message: str) -> str :
+    def __improve_expression(self, message: str) -> str :
         """표현 개선 함수"""
         prompt = [{"role":"system","content":f"당신은 영어 회화 전문가입니다. 다음 영어 표현을 영어권 사람들이 일상적으로 사용하는 표현으로 개선해주세요. 또한 표현을 더 풍부하게 개선해주세요. \n대사 : {message}"}]
-        result = self.invoke(prompt)
-        status,reason = self.evaluate(sentence_to_evaluation=result)
+        result = self.llm.invoke(prompt)
+        status,reason = self.__evaluate(sentence_to_evaluation=result)
 
         if status == "success" :
             prompt.append({"role":"assistant","content":result})
             prompt.append({"role":"user","content":f"당신의 개선본은 다음의 이유로 부적절합니다. 이유를 고려하여 다시 개선해주세요.\n이유: {reason}"})
-            result = self.invoke(prompt)
-            status,reason = self.evaluate(prompt="이전의 나눈 대화를 토대로 다음의 문장이 개선본으로 적절한지 알려주세요.",sentence_to_evaluation=result)
+            result = self.llm.invoke(prompt)
+            status,reason = self.__evaluate(prompt="이전의 나눈 대화를 토대로 다음의 문장이 개선본으로 적절한지 알려주세요.",sentence_to_evaluation=result)
         
         return result
 
-    def save_feedback(self, message: str) -> str:
+    def __save_feedback(self, message: str) -> str:
         """사용자가 피드백한 내용을 저장하기 위한 함수"""
         system_prompt = [{"role": "system", "content": "당신은 사용자의 피드백을 통해 사용자가 현재 대화의 어떤 부분을 어떻게 생각하는지 요약하는 일의 전문가입니다. 다음 제공되는 user와 assistant의 대화 그리고 사용자의 피드백을 토대로 사용자의 생각을 요약해주세요."}]
         full_prompt = system_prompt + self.messages + [{"role": "user", "content": message}]
         
-        feedback = self.invoke(full_prompt)
+        feedback = self.llm.invoke(full_prompt)
         return feedback
 
     
-    def evaluate(self, prompt: str, sentence_to_evaluation: str) -> Tuple[bool,Optional[str]]:
-        """목적에 맞게 잘 행동했는지 평가하기 위한 evaluate 함수"""
+    def __evaluate(self, prompt: str, sentence_to_evaluation: str) -> Tuple[bool,Optional[str]]:
+        """목적에 맞게 잘 행동했는지 평가하기 위한 __evaluate 함수"""
         generator = outlines.Generator(self.evaluate_model,EvaluationResult)
         input = self.messages + [{"role":"system","content":f"{prompt}\n{sentence_to_evaluation}"}]
-        input = self.chat_list_to_str(input)
+        input = self.__chat_list_to_str(input)
         result = json.loads(generator(input, max_new_tokens=128))
         return result["status"],result["reason"]
     
-    def chat_list_to_str(self,chat_list: List[dict]) -> str:
+    def __chat_list_to_str(self,chat_list: List[dict]) -> str:
         """evaluator에 넣기 위해 List 자료형을 str 자료형으로 변환해주는 함수"""
         result = ""
         for chat in chat_list :
@@ -197,24 +180,7 @@ class MainAgent:
         return result
             
 
-    def invoke(self,messages: str, thinking: bool = False) -> str:
-        """추론 단계"""
-        input_ids = self.tokenizer.apply_chat_template(messages,
-                                                       add_generation_prompt=True, 
-                                                       return_tensors="pt",
-                                                       enable_thinking = thinking).to(self.model.device)
-        
-        with torch.no_grad():
-            output = self.model.generate(
-                input_ids,
-                max_new_tokens = len(input_ids)+512,
-                do_sample = False,
-            )
 
-        result = self.tokenizer.decode(output[0],skip_special_tokens=True)
-        result = result.split('</think>')[-1].strip()
-
-        return result
     
 if __name__=="__main__":
     agent = MainAgent() 
