36477d22a2249b3c4cbaf5377b1ce901289a1eb1
kingrangE <yuvw123@gmail.com>
2025-10-02 19:51:19 +0900

    Code : Create Tool Classes
    아래 두가지 기능을 가진 클래스를 생성하였습니다.
    음성 기능을 다루는 VoiceTool (TTS/STT 기능 가짐)
    그래프 관련 기능을 가지는 Neo4jGraphTool (Extract Graph, Create Cyper Query, Upload to sandbox 기능 가짐)

--- Agent/tools.py
+++ Agent/tools.py
@@ -1,50 +1,84 @@
+from llm import LLM
+from typing import Any,Optional
+from prompts import NEExtractionTemplate,Text2CypherTemplate
 import speech_recognition as sr
 from playsound import playsound
-from openai import OpenAI
-from dotenv import load_dotenv
 from gtts import gTTS as gt
+import neo4j
+from neo4j_graphrag.experimental.components.kg_writer import Neo4jWriter
+from neo4j_graphrag.experimental.components.types import Neo4jGraph
 
-load_dotenv()
+class VoiceTool :
+    def __init__(self):
+        r = sr.Recognizer()
+        mic = sr.Microphone()
+    def tts(self, content: str):
+        """전달받은 text를 tts를 거쳐 음성파일을 저장하고 재생"""
+        speech = gt(content,lang="en")
+        speech.save("ai_response.wav")
+        self.__play_audio("ai_response.wav")
 
-r = sr.Recognizer()
-mic = sr.Microphone()
+    def stt(self):
+        """녹음하고, stt를 거쳐 text를 반환"""
+        with sr.Microphone() as source: 
+            print("말해보세요!")
+            audio=r.listen(source,100) 
+        try:
+            transcript=r.recognize_google(audio, language="en") 
+            return transcript
+        except sr.UnknownValueError: 
+            return "다시 말해주세요."
+        except sr.RequestError as e:
+            return  "작동되지 않았습니다. 오류코드: {0}".format(e)
 
-# 음성 녹음 함수
-def record_audio():
-    with mic as source:
-        print("recording...")
-        audio_data = r.listen(source)
-        print("complete!")
-        return audio_data
+    def __play_audio(file_name:str):
+        playsound(file_name)
 
-def play_audio(file_name:str):
-    playsound(file_name)
+class Neo4jGraphTool :
+    def __init__(self):
+        self.extraction_template = NEExtractionTemplate()
+        self.cyper_template = Text2CypherTemplate()
+        self.llm = LLM()
+    def extract_entity(
+            self, 
+            schema:dict[str,Any], 
+            examples:str,
+            text:str, 
+            history:str
+        )-> str:
+        prompt = self.extraction_template.format(schema=schema,
+                             examples=examples,
+                             text=text,
+                             history=history)
+        
+        # prompt가 길어질 수록 많은 노드와 관계가 추출될 수 있으므로 prompt 길이에 따라 max_new_token을 결정하도록 함.
+        if len(prompt)/5 < 512:
+            max_new_token = 512
+        else : 
+            max_new_token = 512
 
-def stt(file_name:str):
-    client = OpenAI()
-    audio_file= open(file_name, "rb")
-
-    transcription = client.audio.transcriptions.create(
-        model="gpt-4o-transcribe", 
-        file=audio_file
-    )
-
-    return transcription
-
-def tts(content: str):
-    speech = gt(content,lang="en")
-    speech.save("ai_response.mp3")
-    play_audio("ai_response.mp3")
-
+        result = self.llm.invoke(prompt,max_new_token=max_new_token) 
+        result = result[result.find('{'):result.rfind('}')+1]
+        return result
+    
+    def get_cyper_query(self,
+                        schema: Optional[str] = None,
+                        examples: Optional[str] = None,
+                        query:str = ""):
+        
+        prompt = self.cyper_template.format(schema=schema,examples=examples,query_text=query)
+        result = self.llm.invoke(prompt,max_new_token=512)
+        return result
+    
+    async def write(self,graph_json:dict):
+        with neo4j.GraphDatabase.driver("bolt://13.219.100.55",auth=("neo4j","potatos-voltages-harpoon")) as driver:
+                writer = Neo4jWriter(driver)
+                graph = Neo4jGraph(
+                    nodes = graph_json["nodes"],
+                    relationships = graph_json["relationships"],
+                )
+                await writer.run(graph)
+                print("저장 완료")
+    
 if __name__ == "__main__":
-    # # 음성 녹음
-    # audio_data = record_audio()
-
-    # # 오디오 파일로 저장
-    # with open("test.wav", "wb") as f:
-    #     f.write(audio_data.get_wav_data())
-
-    # # 오디오 파일 재생
-    # play_audio("test.wav")
-    # print(stt("test.wav"))
-    tts("hello. my name is kilwon. Have you ever studied how to use python?")
+    r=sr.Recognizer() 
